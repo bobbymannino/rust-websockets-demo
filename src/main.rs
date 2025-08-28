504809b68796f82f5bcd6b546a1418ef7a336a4d
Bob Mannino <bobby.mannino@securein.com>
2025-08-28 13:56:29 +0100

    add simple `/health` route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router, response::IntoResponse};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new().route("/health", get(health));
+
+	let addr = SocketAddr::from(([0, 0, 0, 0], 2212));
+	println!("Listening on http://{}", addr);
+
+	 let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health() -> impl IntoResponse {
+	"ok"
 }
